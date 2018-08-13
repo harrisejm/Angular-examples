7598e6db040dd97afeb51b20b427e0346e0b029f
Derek Hammer and Eddie Harris <derek@>
2018-08-13 15:30:46 -0700

    add loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'Angular Homework';
+  testStuff: string = 'Hello, this is a test';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+tasks: Task[] = [
+new Task("Finish weekend Angular homework for Epicodus course", 1),
+new Task("Start tomorrows homework for Epicodus course", 2),
+new Task("Already done", 3),
+new Task("Done", 4),
+new Task("Finish for the week", 5)
+]
+ // firstTask = {
+ //    description: "Finish weekend Angular homework for Epicodus course",
+ //    amountLeft: 5
+ //  }
 }
